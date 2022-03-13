853cb258f22e3dc1500a809bec0a2d5c938da477
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-03-13 11:39:42 +0900

    hotfix: 変数名被ってた

--- chord.py
+++ chord.py
@@ -14,22 +14,22 @@ def main():
     return
   
   #chord created with 3 tones
-  root_tone = 4.0
-  third_tone = 4.0
-  fifth_tone = 4.0
+  root_tone3 = 5.0
+  third_tone3 = 4.0
+  fifth_tone3 = 3.0
 
   #chord created with 4 tones
-  root_tone = 3.0
-  third_tone = 3.0
-  fifth_tone = 3.0
-  seventh_tone = 3.0
+  root_tone4 = 3.0
+  third_tone4 = 3.0
+  fifth_tone4 = 3.0
+  seventh_tone4 = 3.0
 
-  template_major = np.roll(np.array([root_tone, 0.0, 0.0, 0.0, third_tone, 0.0, 0.0, fifth_tone, 0.0, 0.0, 0.0, 0.0]), key_shift)
-  template_minor = np.roll(np.array([root_tone, 0.0, 0.0, third_tone, 0.0, 0.0, 0.0, fifth_tone, 0.0, 0.0, 0.0, 0.0]), key_shift)
-  template_dim = np.roll(np.array([root_tone, 0.0, 0.0, third_tone, 0.0, 0.0, fifth_tone, 0.0, 0.0, 0.0, 0.0, 0.0]), key_shift)
-  template_aug = np.roll(np.array([root_tone, 0.0, 0.0, 0.0, third_tone, 0.0, 0.0, 0.0, fifth_tone, 0.0, 0.0, 0.0]), key_shift)
-  template_sus4 = np.roll(np.array([root_tone, 0.0, 0.0, 0.0, 0.0, third_tone, 0.0, fifth_tone, 0.0, 0.0, 0.0, 0.0]), key_shift)
-  template_sus2 = np.roll(np.array([root_tone, 0.0, third_tone, 0.0, 0.0, 0.0, 0.0, fifth_tone, 0.0, 0.0, 0.0, 0.0]), key_shift)
+  template_major = np.roll(np.array([root_tone3, 0.0, 0.0, 0.0, third_tone3, 0.0, 0.0, fifth_tone3, 0.0, 0.0, 0.0, 0.0]), key_shift)
+  template_minor = np.roll(np.array([root_tone3, 0.0, 0.0, third_tone3, 0.0, 0.0, 0.0, fifth_tone3, 0.0, 0.0, 0.0, 0.0]), key_shift)
+  template_dim = np.roll(np.array([root_tone3, 0.0, 0.0, third_tone3, 0.0, 0.0, fifth_tone3, 0.0, 0.0, 0.0, 0.0, 0.0]), key_shift)
+  template_aug = np.roll(np.array([root_tone3, 0.0, 0.0, 0.0, third_tone3, 0.0, 0.0, 0.0, fifth_tone3, 0.0, 0.0, 0.0]), key_shift)
+  template_sus4 = np.roll(np.array([root_tone3, 0.0, 0.0, 0.0, 0.0, third_tone3, 0.0, fifth_tone3, 0.0, 0.0, 0.0, 0.0]), key_shift)
+  template_sus2 = np.roll(np.array([root_tone3, 0.0, third_tone3, 0.0, 0.0, 0.0, 0.0, fifth_tone3, 0.0, 0.0, 0.0, 0.0]), key_shift)
   templates = np.array([np.roll(template_major, k) for k in range(0, 12)] + [np.roll(template_minor, k) for k in range(0, 12)] + \
     [np.roll(template_dim, k) for k in range(0, 12)] + [np.roll(template_aug, k) for k in range(0, 12)] + \
     [np.roll(template_sus4, k) for k in range(0, 12)] + [np.roll(template_sus2, k) for k in range(0, 12)])
@@ -65,8 +65,10 @@ def main():
 
   before_chord_num: int = -1
   chord: str = ""
+
   for i in range(0, len(chord_data[0])):
     k = np.argmax(chord_data[:,i])
+
     chord_name = "C" if k == 0 else "C#" if k == 1 else "D" if k == 2 else "D#" if k == 3 else "E" if k == 4 else "F" if k == 5 else "F#" if k == 6 else "G" if k == 7 else "G#" if k == 8 else "A" if k == 9 else "A#" if k == 10 else "B" if k == 11 else \
     "Cm" if k == 12 else "Cm#" if k == 13 else "Dm" if k == 14 else "Dm#" if k == 15 else "Em" if k == 16 else "Fm" if k == 17 else "Fm#" if k == 18 else "Gm" if k == 19 else "Gm#" if k == 20 else "Am" if k == 21 else "Am#" if k == 22 else "Bm" if k == 23 else \
     "Cdim" if k == 24 else "C#dim" if k == 25 else "Ddim" if k == 26 else "D#dim" if k == 27 else "Edim" if k == 28 else "Fdim" if k == 29 else "F#dim" if k == 30 else "Gdim" if k == 31 else "G#dim" if k == 32 else "Adim" if k == 33 else "A#dim" if k == 34 else "Bdim" if k == 35 else \
